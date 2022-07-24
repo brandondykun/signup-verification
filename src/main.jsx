bf8a73991e2450eb3f50d8ffd67c93af90da3d20
Brandon Dykun <brandon.dykun@gmail.com>
2022-07-24 07:50:02 -0400

    Removed basename from BrowserRouter.

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import { UserContextProvider } from "./context/UserContext";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <UserContextProvider>
-      <BrowserRouter basename="/signup-verification/">
+      <BrowserRouter>
         <App />
       </BrowserRouter>
     </UserContextProvider>
