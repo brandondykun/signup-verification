4ab728f74a9eb7b6dfb3752dcdf6a5ae3e689a0e
Brandon Dykun <brandon.dykun@gmail.com>
2022-07-24 00:43:48 -0400

    Created main basic logic for component.

--- signup-verification/src/pages/Confirm.jsx
+++ signup-verification/src/pages/Confirm.jsx
@@ -1,7 +1,19 @@
+import { useUserContext } from "../context/UserContext";
+import { Link } from "react-router-dom";
+
 const ConfirmPage = () => {
+  const { contextUser, contextPassword } = useUserContext();
+
   return (
-    <div>
-      <h1>Confirm Page</h1>
+    <div className="sign-up-card">
+      <h1 className="title">Account Details</h1>
+      <div className="user-detail-container">
+        <div>Username: {contextUser}</div>
+        <div>Password: {contextPassword}</div>
+      </div>
+      <Link to="/signup/">
+        <button className="form-button submit">back</button>
+      </Link>
     </div>
   );
 };
